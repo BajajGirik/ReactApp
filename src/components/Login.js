18f2397efccb43cf520d8e1b817cc489970de0f5
Girik Bajaj <bajajgirik2010@gmail.com>
2021-07-13 13:07:35 +0530

    Styled login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,18 +5,30 @@ import { useRef } from 'react';
 function Login() {
     let history = useHistory();
     const emailref = useRef(null);
+    const passref = useRef(null);
 
-    const signIn = () => {
+    const signIn = (e) => {
+        e.preventDefault();
 
-        history.push("/home");
+        if (emailref.current.value === "admin@gmail.com" && passref.current.value === "admin")
+            history.push("/");
+        else
+            alert("Credentials Not Valid");
     }
 
     return (
         <div className="Login">
-            <form onSubmit={signIn}>
-                <input type="email" placeholder="Email ID" name="Email" />
-                <input type="password" placeholder="Password" name="Pass" />
-                <button type="submit">Login</button>
+            <form onSubmit={signIn} className="Login__form">
+                <h1>LOGIN</h1>
+                <div className="Login__inpuContainer">
+                    <div className="fa fa-envelope"></div>
+                    <input ref={emailref} type="email" placeholder="Email ID" name="Email" required />
+                </div>
+                <div className="Login__inpuContainer">
+                    <div className="fa fa-key"></div>
+                    <input ref={passref} type="password" placeholder="Password" name="Pass" required/>
+                </div>
+                <button className="Login__button" type="submit">Login</button>
             </form>
         </div>
     )
